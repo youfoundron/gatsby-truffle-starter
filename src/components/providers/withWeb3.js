e763012039a7ee307a3f810e990d908ffa22c3ea
Ron Gierlach <rongierlach@gmail.com>
2017-08-08 19:06:30 -0500

    add web3 provider

---
+++ src/components/providers/withWeb3.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { compose, bindActionCreators } from 'redux'
+
+import getWeb3 from '../../util/getWeb3'
+import { setWeb3 } from '../../state/constants/actionCreators'
+
+const withWeb3 = Cmp => class extends Component {
+  componentWillMount () {
+    console.log('will initialize web3')
+    const { web3, setWeb3 } = this.props
+    if (!web3) getWeb3.then(setWeb3)
+  }
+
+  render () {
+    const { web3, setWeb3, ...props } = this.props
+    return web3
+      ? <Cmp web3={web3} {...props} />
+      : null
+  }
+}
+
+const mapStateToProps = state => ({
+  web3: state.web3
+})
+
+const mapDispatchToProps = dispatch => ({
+  setWeb3: bindActionCreators(setWeb3, dispatch)
+})
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withWeb3
+)
