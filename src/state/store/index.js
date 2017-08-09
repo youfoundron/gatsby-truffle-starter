77f1ab69c6d97f880b79f3fb07ba3978954d6d5f
Ron Gierlach <rongierlach@gmail.com>
2017-08-09 14:03:01 -0500

    comment out the redux-logger middleware

--- src/state/store/index.js
+++ src/state/store/index.js
@@ -15,13 +15,13 @@ const actionSanitizer = action => ({
 })
 
 const middlewares = []
-if (process.env.NODE_ENV !== 'production') {
-  middlewares.push(createLogger({
-    diff: true,
-    collapsed: true,
-    actionTransformer: actionSanitizer
-  }))
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   middlewares.push(createLogger({
+//     diff: true,
+//     collapsed: true,
+//     actionTransformer: actionSanitizer
+//   }))
+// }
 
 const middleware = applyMiddleware(...middlewares)
 
