ee3ff0f7be4ff26fb3ced962d73317b2dc837f47
Ron Gierlach <rongierlach@gmail.com>
2017-08-08 20:40:25 -0500

    add static favicon

--- src/html.js
+++ src/html.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import favicon from './favicon.ico'
+
+console.log({favicon})
 
 let styles
 if (process.env.NODE_ENV === 'production') {
@@ -15,6 +18,7 @@ const Html = ({ headComponents, body, postBodyComponents }) => (
       <meta charSet='utf-8' />
       <meta httpEquiv='x-ua-compatible' content='ie=edge' />
       <meta name='viewport' content='width=device-width, initial-scale=1.0, shrink-to-fit=no' />
+      <link rel='icon' type='image/x-icon' href={`${__PATH_PREFIX__}/favicon.ico`} />
       { headComponents }
       { process.env.NODE_ENV === 'production'
         ? <style
