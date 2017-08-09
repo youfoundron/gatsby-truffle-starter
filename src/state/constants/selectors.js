f8047f8932d2a446bb54dd238827503038996ea7
Ron Gierlach <rongierlach@gmail.com>
2017-08-09 12:54:40 -0500

    add selector to detect if contracts are deployed

--- src/state/constants/selectors.js
+++ src/state/constants/selectors.js
@@ -5,3 +5,9 @@ export const hasAccounts = ({ accounts }) => !!(
 export const getCoinbase = ({ accounts: [coinbase] }) => (
   coinbase
 )
+
+export const contractsAreDeployed = ({ contracts }, contractPairs) => {
+  for (let [name] of contractPairs)
+    if (typeof contracts[name] !== 'object') return false
+  return true
+}
