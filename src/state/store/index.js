3dca6ee5ee177786fc59ed954edd3b83f0417bab
Ron Gierlach <rongierlach@gmail.com>
2017-08-09 13:44:49 -0500

    replace redux-devtools w/ redux-logger

--- src/state/store/index.js
+++ src/state/store/index.js
@@ -1,14 +1,9 @@
 import { compose, createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
 
 import rootReducer from '../reducers'
 import initialState from './initialState'
 import * as actionCreators from '../constants/actionCreators'
-
-// support redux devtools extension
-const useReduxDevtools =
-  process.env.NODE_ENV !== 'production' &&
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 
 // middleware helpers
 const typeRegEx = /Symbol\((.*?)\)/
@@ -19,18 +14,16 @@ const actionSanitizer = action => ({
     : action.type
 })
 
-const composeEnhancers = useReduxDevtools
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-    actionCreators,
-    actionSanitizer
-  })
-  : compose
-
 const middlewares = []
-const middleware = composeEnhancers(
-  applyMiddleware(...middlewares)
-  // additional enhancers...
-)
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(createLogger({
+    diff: true,
+    collapsed: true,
+    actionTransformer: actionSanitizer
+  }))
+}
+
+const middleware = applyMiddleware(...middlewares)
 
 // create the store
 const store = createStore(rootReducer, initialState, middleware)
