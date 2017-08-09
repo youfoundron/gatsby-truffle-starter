4a9023c4d15c53caa42e27b44a868d96d2d1b31b
Ron Gierlach <rongierlach@gmail.com>
2017-08-09 13:03:35 -0500

    basic styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { compose } from 'redux'
 
+import '../styles/index.css'
 import withWeb3 from '../components/providers/withWeb3'
 import withAccounts from '../components/providers/withAccounts'
-// import withContracts from '../components/providers/withContracts'
+import withContracts from '../components/providers/withContracts'
 
 const Index = ({ accounts, contracts }) => {
   return (
     <main>
-      <h1>Hello World</h1>
+      <h2>MetaCoin</h2>
+      <hr />
+      <p><label>Coinbase: </label>{accounts[0]}</p>
+
     </main>
   )
 }
@@ -16,5 +20,5 @@ const Index = ({ accounts, contracts }) => {
 export default compose(
   withWeb3,
   withAccounts,
-  // withContracts
+  withContracts
 )(Index)
