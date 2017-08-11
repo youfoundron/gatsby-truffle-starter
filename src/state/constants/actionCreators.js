573f1bdd35f3861b47f54f59eca0c43060e40fa3
Ron Gierlach <rongierlach@gmail.com>
2017-08-10 21:06:33 -0500

    add more actions

--- src/state/constants/actionCreators.js
+++ src/state/constants/actionCreators.js
@@ -14,3 +14,27 @@ export const setContracts = contracts => ({
   type: types.SET_CONTRACTS,
   payload: contracts
 })
+
+export const resetForm = () => ({
+  type: types.RESET_FORM
+})
+
+export const updateRecipient = e => ({
+  type: types.UPDATE_RECIPIENT,
+  payload: e.target.value
+})
+
+export const updateAmount = e => ({
+  type: types.UPDATE_AMOUNT,
+  payload: e.target.value
+})
+
+export const updateBalance = balance => ({
+  type: types.UPDATE_BALANCE,
+  payload: balance
+})
+
+export const pushTransaction = txReceipt => ({
+  type: types.PUSH_TRANSACTION,
+  payload: txReceipt
+})
