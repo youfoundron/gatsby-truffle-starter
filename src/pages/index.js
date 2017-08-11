818c496462c0fedb94f13eb20066e9dd9cf9ed65
Ron Gierlach <rongierlach@gmail.com>
2017-08-10 21:08:16 -0500

    add components to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { compose } from 'redux'
 
+/* Styles
+** ****************** */
 import '../styles/index.css'
+
+/* Components
+** ****************** */
+import CoinbaseLabel from '../components/CoinbaseLabel'
+import MetaCoinBalance from '../components/MetaCoinBalance'
+import MetaCoinForm from '../components/MetaCoinForm'
+import TransactionsList from '../components/TransactionsList'
+
+/* Providers
+** ****************** */
 import withWeb3 from '../components/providers/withWeb3'
 import withAccounts from '../components/providers/withAccounts'
 import withContracts from '../components/providers/withContracts'
 
-const Index = ({ accounts, contracts }) => {
-  return (
-    <main>
-      <h2>MetaCoin</h2>
-      <hr />
-      <p><label>Coinbase: </label>{accounts[0]}</p>
-
-    </main>
-  )
-}
+const Index = () => (
+  <main>
+    <h2>Transfer MetaCoins</h2>
+    <hr />
+    <CoinbaseLabel />
+    <MetaCoinBalance />
+    <MetaCoinForm />
+    <TransactionsList />
+  </main>
+)
 
 export default compose(
   withWeb3,
