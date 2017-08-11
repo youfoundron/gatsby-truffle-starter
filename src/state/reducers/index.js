42f8f4f979419bc329d8b111dc39c5ed87ce2aa5
Ron Gierlach <rongierlach@gmail.com>
2017-08-10 21:07:23 -0500

    update root reducer

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux'
 import web3 from './web3'
+import meta from './meta'
+import formData from './formData'
 import accounts from './accounts'
 import contracts from './contracts'
 
 export default combineReducers({
   web3,
+  meta,
+  formData,
   accounts,
   contracts
 })
