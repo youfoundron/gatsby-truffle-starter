2fb8d1edd58211197d570aeb67b75c4b655b4881
Ron Gierlach <rongierlach@gmail.com>
2017-08-10 21:06:17 -0500

    add more action types

--- src/state/constants/actionTypes.js
+++ src/state/constants/actionTypes.js
@@ -13,6 +13,19 @@ const appTypes = [
   'SET_CONTRACTS'
 ]
 
+const formTypes = [
+  'RESET_FORM',
+  'UPDATE_RECIPIENT',
+  'UPDATE_AMOUNT',
+]
+
+const metaTypes = [
+  'UPDATE_BALANCE',
+  'PUSH_TRANSACTION'
+]
+
 export default {
-  ...scopeTypes(appTypes)
+  ...scopeTypes(appTypes),
+  ...scopeTypes(formTypes, 'Form'),
+  ...scopeTypes(metaTypes, 'Meta')
 }
