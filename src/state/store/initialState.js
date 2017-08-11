8f1a6ffdd5e625fcac28d90a52af5982206fcbc2
Ron Gierlach <rongierlach@gmail.com>
2017-08-10 21:07:38 -0500

    update initialState

--- src/state/store/initialState.js
+++ src/state/store/initialState.js
@@ -1,5 +1,13 @@
 export default {
   web3: null,
   accounts: [],
-  contracts: {}
+  contracts: {},
+  meta: {
+    balance: 0,
+    transactions: []
+  },
+  formData: {
+    amount: 0,
+    recipient: ''
+  },
 }
